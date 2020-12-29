5c7cde1e5f2537e934ea9af2cbfe629cc34281f6
julioam22 <julioam22@gmail.com>
2020-12-29 15:44:21 -0300

    Commit Inicial

---
+++ server/server.js
@@ -0,0 +1,51 @@
+require('./config/config.js')
+const express = require('express')
+const app = express()
+
+var bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', function(req, res) {
+    res.json('Hello World')
+})
+
+app.get('/usuario', function(req, res) {
+    res.json('USUARIO');
+})
+
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+    } else {
+        res.json({ persona: body });
+    }
+
+})
+
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    res.json({
+        id
+    });
+})
+
+
+app.delete('/usuario', function(req, res) {
+    res.json('USUARIO delete')
+})
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Escucjando puerto ', process.env.PORT);
+});
